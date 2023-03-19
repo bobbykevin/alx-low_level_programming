02bac42368db3f34f29845bf12b908dd7e22cf0f
Akinbobola Akinyemi <123414751+bobbykevin@users.noreply.github.com>
2023-03-19 23:09:17 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,10 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -23,6 +24,7 @@ int main(void)
 	else
 	{
 		printf("%d is negative\n", n);
-	} return (
-			0);
+	} 
+	
+	return (0);
 }
