5444a2b353e57dc11dbf48f809dea42dd11c308f
Akinbobola Akinyemi <123414751+bobbykevin@users.noreply.github.com>
2023-03-25 18:32:56 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,8 @@ void print_alphabet(void) {
 		char c;
 
 			for (c = 'a'; c <= 'z'; c++) {
-				if (c == 'e' || c == 'q') {
-            continue;
-				}
-						_putchar(c);
+				
+				_putchar(c);
 	}
 	_putchar('\n');
 }
