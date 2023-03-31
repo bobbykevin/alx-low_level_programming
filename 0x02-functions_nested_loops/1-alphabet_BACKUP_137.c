e781b8a13d2830d535a1923156a3c0e3fb87aa8b
bobbykevin <bobbykevin32@live.com>
2023-03-30 17:24:01 -0700

    Update

---
+++ 0x02-functions_nested_loops/1-alphabet_BACKUP_137.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+ Updated upstream
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+
+		char c;
+			for (c = 'a'; c <= 'z'; c++)
+					{
+								_putchar(c);
+									}
+			_putchar('\n');
+ Stashed changes
+}
