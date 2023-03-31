f3c607f596a17984614faa60fbda86f4b78b4be6
bobbykevin <bobbykevin32@live.com>
2023-03-31 15:37:54 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,34 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
  * leet - encode into 1337speak
  * @n: input value
  * Return: n value
  */
-char *leet(char *str)
+char *leet(char *n)
 {
-	  char *ptr;
-	  char leet_str[strlen(str) + 1];
-	      for (ptr = str; *ptr != '\0'; ptr++)
-	      {
-		      leet_str[ptr - str] = *ptr;
-		      if (*ptr == 'a' || *ptr == 'A')
-			      leet_str[ptr - str] = '4';
-		      for (int i = 0; i < 2; i++)
-		      {
-			      if (*ptr == 'e' || *ptr == 'E')
-				      leet_str[ptr - str] = '3';
-			      if (*ptr == 'o' || *ptr == 'O')
-				      leet_str[ptr - str] = '0';
-			      if (*ptr == 't' || *ptr == 'T')
-				      leet_str[ptr - str] = '7';
-			      if (*ptr == 'l' || *ptr == 'L')
-				      leet_str[ptr - str] = '1';
-		      }
-	      }
-	      leet_str[ptr - str] = '\0';
-	      strcpy(str, leet_str);
-	      return str;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
 }
 
